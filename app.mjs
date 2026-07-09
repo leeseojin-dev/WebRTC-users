b43d7e6f04666a6d2709aa174c296ae90ef1ca84
LeeSeoJin <dltjwls5669@naver.com>
2026-07-09 21:28:47 +0900

    express.static 경로 -> 절대경로로 수정

--- app.mjs
+++ app.mjs
@@ -1,12 +1,18 @@
 import express from "express"
+import path from "path"
+import { fileURLToPath } from "url"
 import { config } from "./config.mjs"
 import { connectDB } from "./db/user_database.mjs"
 import authRouter from "./router/auth.mjs"
 
 const app = express()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 app.use(express.json())
-app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, "public")))
+
 app.use("/auth", authRouter)
 
 app.use((req, res) => {
